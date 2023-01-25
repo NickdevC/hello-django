ee2c4a53eb5ce96caeb15a609701e8ac7b0384e6
NickdevC <nick_coles@msn.com>
2023-01-25 18:57:41 +0000

    Add development = true

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 if os.path.isfile('env.py'):
     import env
 SECRET_KEY = os.environ.get('SECRET_KEY')
+development = os.environ.get('DEVELOPMENT', True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
